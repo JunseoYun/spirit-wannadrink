7623c9fe9118f35bba4ae4f20e13f2f364b991f8
Junseo Yun <yunjunseo@naver.com>
2026-06-13 01:50:54 +0900

    Fixed : 기본 앱 설정

--- granite.config.ts
+++ granite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from '@apps-in-toss/web-framework/config';
+import { defineConfig } from "@apps-in-toss/web-framework/config";
 
 export default defineConfig({
-  appName: 'spirit-wannadrink',
+  appName: "spirit-wannadrink",
   brand: {
-    displayName: 'spirit-wannadrink', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
-    primaryColor: '#3182F6', // 앱의 기본 색상으로 바꿔주세요.
-    icon: '', // 콘솔에서 등록한 아이콘 이미지 주소로 바꿔주세요.
+    displayName: "한잔할까", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+    primaryColor: "#3182F6", // 앱의 기본 색상으로 바꿔주세요.
+    icon: "https://static.toss.im/appsintoss/32779/2f94ba49-f1ee-4845-8bf5-1765be80f567.png", // 콘솔에서 등록한 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
-    host: '192.168.45.54',
+    host: "192.168.45.134",
     port: 5173,
     commands: {
-      dev: 'vite --host',
-      build: 'vite build',
+      dev: "vite --host",
+      build: "vite build",
     },
   },
   permissions: [],
